195f2a3bf00d454dc5522c32b40c865beb727c63
Darius <jonat496@gmail.com>
2021-05-25 21:41:07 +0700

    Login & Design
    Login Logout
    Design

--- Aplikasi_SPK/urls.py
+++ Aplikasi_SPK/urls.py
@@ -1,25 +1,14 @@
-"""Aplikasi_SPK URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from aplikasi.views import *
+from django.contrib.auth.views import *
 
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
     path('index/', index, name='index'),
     path('index/ubah_data/<int:id_karyawan>', ubah_data,name='ubah'),
     path('index/hapus_data/<int:id_karyawan>', hapus_data, name='hapus'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- aplikasi/views.py
+++ aplikasi/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from aplikasi.models import *
 from aplikasi.form import *
+from django.conf import settings
 from django.shortcuts import *
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url=settings.LOGIN_URL)
 def index(request):
     karyawan = Karyawan.objects.all()
     if request.POST:
@@ -28,7 +30,7 @@ def index(request):
 
     return render(request,'test.html', konteks)
 
-
+@login_required(login_url=settings.LOGIN_URL)
 def ubah_data(request, id_karyawan):
     karyawan = Karyawan.objects.get(id=id_karyawan)
     if request.POST:
@@ -45,7 +47,7 @@ def ubah_data(request, id_karyawan):
         }
     return render(request,'ubah_data.html',konteks)
 
-
+@login_required(login_url=settings.LOGIN_URL)
 def hapus_data(request,id_karyawan):
     karyawan = Karyawan.objects.filter(id=id_karyawan)
     karyawan.delete()
